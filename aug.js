aebf32d080d8b516f9770c2fcc90ea41ead1a2ba
Daniel Pelstring <danielpelstring@macbookpro.lan>
2025-03-04 19:59:51 -0500

    created CSS and JSON files for the main story pages.  Also created js files for alerts to be used on non main pages

---
+++ aug.js
@@ -0,0 +1,3 @@
+function augEight {
+  alert("Photographer Iain MacMillan takes a series of pictures of The Beatles crossing the street. One would eventually become the cover of their final recording, 'Abbey Road'")
+}
