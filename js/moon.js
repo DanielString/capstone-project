9f4cf8e869a8a2730c9950b7c3006a1bf38b1380
Daniel Pelstring <danielpelstring@Daniels-MacBook-Pro.local>
2025-03-12 13:05:04 -0400

    completed JavaScript for the remaining story pages.  Quotes mow populate as desired

---
+++ js/moon.js
@@ -0,0 +1,28 @@
+async function moon() {
+  let quoteDiv = document.getElementById('quote');
+  try {
+      const response = await fetch('../json/moon.json');
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const jsonData = await response.json();
+      console.log('JSON data:', jsonData);
+
+      //Pick random quote
+      const randomIndex = Math.floor(Math.random() * jsonData.length);
+      const randomQuote = jsonData[randomIndex];
+
+      //Dipslay Quote
+      quoteDiv.textContent = `"${randomQuote.quote}" - ${randomQuote.source}`;
+
+  } catch (error) {
+      console.error('An error occurred:', error.message);
+  }
+};
+
+moon();
+
+// Refresh quote every hour
+setInterval(wood, 360000);
