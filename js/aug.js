479b0b57a1712abd5773e515d28c96f399c158dc
Daniel Pelstring <danielpelstring@macbookpro.lan>
2025-03-23 22:27:59 -0400

    added JS alerts to the august and june JS files which will be added to the calendar pages

--- js/aug.js
+++ js/aug.js
@@ -1,3 +1,111 @@
+function augOne() {
+  alert("Three newspapers (The Vallejo Times Herald, The San Francisco Chronicle and The San Francisco Examiner) recieve a letter from an unknown person claiming to have killed 3 persons.  He would become known as 'The Zodiac Killer.")
+}
+
+function augTwo() {
+  alert("Richard Nixon becomes the first US President to travel to a communist country when he arrives in Romania as a guest of Romanian President Nicolae Ceaușescu.")
+}
+
+function augThree() {
+  alert("Coronado Bridge officially opens in San Diego, CA.")
+}
+
+function augFour() {
+  alert("National Security Adviser Henry Kissinger secretly met with North Vietnam's former Foreign Minister, Xuan Thuy, to discuss a means of settling the Vietnam War at the Paris Peace Talks.")
+}
+
+function augFive() {
+  alert("Mariner 7 transmitted the closest pictures taken, up to that time, of the planet Mars, after the American interplanetary probe came within 2,000 miles (3,200 km) of the Martian surface.")
+}
+
+function augSix() {
+  alert("A sonic boom from a U.S. Navy F-4J Phantom II jet aircraft shattered three-quarters of the windows in the downtown business district of the Canadian resort town of Kelowna, British Columbia, causing an estimated $150,000 worth of damage and injuring six people hurt by broken glass.")
+}
+
+function augSeven() {
+  alert("NASA officials announced at the Ames Research Center in Houston that testing of the lunar soil, brought back by the Apollo 11 mission, showed no organisms and no 'positive traces of life'.")
+}
+
 function augEight() {
   alert("Photographer Iain MacMillan takes a series of pictures of The Beatles crossing the street. One would eventually become the cover of their final recording, 'Abbey Road'")
+}
+
+function augEleven() {
+  alert("The United States Senate voted unanimously, 91 to 0, to restrict further production, development and testing by the United States military of chemical weapons and biological weapons.")
+}
+
+function augTwelve() {
+  alert("By a vote of 4 to 1, the U.S. Federal Trade Commission (FTC) voted to issue the first regulations to control 'giveaway' games of chance that had been used in promotions by supermarket and gasoline station chains since 1964, with the lone dissenter objecting on grounds that 'promotional gimmicks are inherently unfair and should be outlawed entirely'.")
+}
+
+function augThirteen() {
+  alert("The Apollo 11 astronauts rode in ticker-tape parades in their honor in New York and Chicago, with an estimated six million attendees.")
+}
+
+function augFourteen() {
+  alert("British Army troops began what would become an almost 38-year presence in Northern Ireland, the companion to Great Britain in the United Kingdom, as more than 300 soldiers from the Prince of Wales Regiment arrived at Derry to end three days of rioting called the 'Battle of the Bogside' and to maintain security there.")
+}
+
+function augSixteen() {
+  alert("A week after the 'Tate Murders', Charles Manson and members of his 'Family' are arrested in a raid on the Spahn Movie Ranch. Charged with auto-theft, they are released three days later.")
+}
+
+function augSeventeen() {
+  alert("Hurricane Camille made landfall on the Mississippi coast as Biloxi and other coastal cities were evacuated. A large number of people, however, chose to 'ride out the storm' and even held parties over the weekend to celebrate the excitement. In all, 259 people would be killed as a direct result of the hurricane and the flooding produced by the storms.")
+}
+
+function augEighteen() {
+  alert("Long John Silver's opened its first store, making its debut in Lexington, Kentucky.")
+}
+
+function augNineteen() {
+  alert("Hurricane Camille caused its highest loss of life far inland from sea, killing 124 people in Nelson County, Virginia. During the night, more than 30 inches (760 mm) of rain fell in a few hours, causing flash floods and mudslides.")
+}
+
+function augTwenty() {
+  alert("The Beatles assembled at Abbey Road Studios to complete the last necessary production for their album 'Abbey Road'.")
+}
+
+function augTwentyOne() {
+  alert("Miles Davis has the final recording session for his landmark jazz/rock fusion album, 'Bitches Brew'.")
+}
+
+function augTwentyTwo() {
+  alert("Tran Van Huong was fired as Prime Minister of South Vietnam by President Nguyen Van Thieu.")
+}
+
+function augTwentyThree() {
+  alert("Johnny Cash's song 'A Boy Named Sue' hit #1 on the charts.")
+}
+
+function augTwentyFour() {
+  alert("The first publicized revolt of American soldiers in the Vietnam War took place when 'A' Company of the 196th Brigade of the Americal Division refused to obey the orders of the U.S. Army lieutenant who was commanding them.")
+}
+
+function augTwentyFive() {
+  alert("Three weeks after a letter from President Nixon's to North Vietnam's President Ho Chi Minh was sent, representatives of the ailing 79-year old North Vietnamese leader sent a reply that would be described as 'uncompromising'; a historian would later write that 'Ho neither read Nixon's letter nor did he pen the reply'")
+}
+
+function augTwentySix() {
+  alert("Stuntman and ranch hand Donald 'Shorty' Shea was murdered by the followers of Charles Manson after the group had been released from jail following an August 16 raid on the Spahn Ranch.")
+}
+
+function augTwentySeven() {
+  alert("Two U.S. military policemen were killed in a shooting incident at a nightclub in Saigon, stemming from a conflict with an ARVN (Army of the Republic of Vietnam) Lieutenant colonel.")
+}
+
+function augTwentyEight() {
+  alert("Actor/Comedian/Musician Jack Black is born in Santa Monica, California")
+}
+
+function augTwentyNine() {
+  alert("The IMP, a crucial component for the ARPANET (the precursor to the internet), was sent to UCLA, paving the way for the first data transfer between a host computer and the network switch on September 2.")
+}
+
+function augThirty() {
+  alert("The Texas International Pop Festival opens near Dallas. Performers include Janis Joplin, B.B. King, Santana, and Led Zeppelin.")
+}
+
+function augThirtyOne() {
+  alert("Rocky Marciano, the only undefeated world heavyweight boxing champion in history, was killed in the crash of a Cessna 172 airplane that was taking him from Chicago, IL to Des Moines,IA.")
 }
---
+++ js/jun.js
@@ -0,0 +1,115 @@
+function junOne() {
+  alert("Tobacco advertising is banned on Canadian radio and TV.")
+}
+
+function junTwo() {
+  alert("The United States Supreme Court voted to set aside a death sentence in the case of Boykin v. Alabama, ruling that Edward Boykin's guilty plea was made without him being aware of its consequences.")
+}
+
+function junThree() {
+  alert("The 79th and last original episode of Star Trek, 'Turnabout Intruder', aired on NBC.")
+}
+
+function junFour() {
+  alert("Renowned session keyboardist Nicky Hopkins departed from the 'Jeff Beck Group'. Two months later he would appear at Woodstock with 'Jefferson Airplane'.")
+}
+
+function junFive() {
+  alert("'Rivet Amber', a U.S. Air Force RC-135E reconnaissance spy plane, disappeared along with all 19 of its crew while flying an exercise over the Aleutian Islands in Alaska. Although the plane was not shot down, it sent a distress call to Elmendorf Air Force Base as it traveled between the island of Shemya and Eielson Air Force Base, and was believed to have been monitoring Soviet radar and radio communications. No trace of the aircraft or its crew has ever been found.")
+}
+
+function junSix() {
+  alert("New York Jets quarterback Joe Namath announces that he is retiring, less than six months after being the MVP of Super Bowl III, after a threatened suspension by pro football commissioner Pete Rozelle. Namath had been ordered to sell his one-half interest in a New York restaurant, Bachelors III, because some of the restaurant's regular customers were bookies who were taking gambling bets on the restaurant premises and others were members of organized crime.")
+}
+
+function junSeven() {
+  alert("United States Marine Dan Bullock was killed while guarding the An Hoa Combat Base in South Vietnam. Bullock, an African-American living in Brooklyn, had been only 14 years old when he altered his birth certificate to show that he was 18. He was just 15 when he died, becoming the youngest US Serviceman killed in the Vietnam War.")
+}
+
+function junEight() {
+  alert("Following a meeting at Midway Island between President Richard Nixon and South Vietnamese President Nguyen Van Thieu, Nixon announced that 25,000 American troops would be withdrawn from Vietnam by the end of September.")
+}
+
+function junNine() {
+  alert("President Nixon's nomination of Warren E. Burger for Chief Justice of the United States was confirmed by the Democrat-dominated United States Senate")
+}
+
+function junTen() {
+  alert("The U.S. Air Force donated the first North American Aviation X-15 to the Smithsonian Institution for display at the National Air and Space Museum.")
+}
+
+function junEleven() {
+  alert("Actor Peter Dinklage is born in Morristown, NJ.")
+}
+
+function junTwelve() {
+  alert("NASA announced that Apollo 11 was cleared for launch on July 16 for the first manned landing on the Moon.")
+}
+
+function junThirteen() {
+  alert("The 'Amen Break', a 6-second drum solo that would become one of the most sampled musical tracks of all time,was recorded. Drummer Gregory C. Coleman of 'The Winstons' performed the 4-bar beat 86 seconds into the song 'Amen, Brother'.")
+}
+
+function junFourteen() {
+  alert("22 time Grand Slam winner, Steffi Graf, is born in Mannheim, in then West Germany")
+}
+
+function junFifteen() {
+  alert("'Hee Haw', a show aimed at fans of country music, aired for the first time on the CBS")
+}
+
+function junSixteen() {
+  alert("The US Supreme Court rules that the suspension of Adam Clayton Powell, Jr from House of Representatives violated Article I of the Constitution.")
+}
+
+function junSeventeen() {
+  alert("The avant-garde play 'Oh! Calcutta!' opens in NYC (Almost the entire play is performed by actors in the nude.)")
+}
+
+function junEighteen() {
+  alert("'The Wild Bunch', starring William Holden and Ernest Borgnine, is released.")
+}
+
+function junNinteen() {
+  alert("The House of Representatives voted to ban health warnings in cigarette ads for six years.")
+}
+
+function junTwenty() {
+  alert("Georges Pompidou is sworn in as President of France.")
+}
+
+function junTwentyOne() {
+  alert("American tennis player Maureen Connolly, a 9 time Grand Slam singles winner, dies of ovarian cancer at 34.")
+}
+
+function junTwentyTwo() {
+  alert("The Cuyahoga River in Cleveland, OH, caught fire after an oil slick floating on the river ignited.")
+}
+
+function junTwentyThree() {
+  alert("Boxer Joe Frazier TKOs Jerry Quarry in the eighth round to win the heavyweight title.")
+}
+
+function junTwentyFour() {
+  alert("Journalist/TV and Radio Host Rich Eisen is born in Brooklyn, NY.")
+}
+
+function junTwentyFive() {
+  alert("American Pancho Gonzalez beats Puerto Rican Charlie Pasarell in a 5 hour and 12 minute, 112-game marathon at Wimbledon.")
+}
+
+function junTwentySix() {
+  alert("A former NASA official told reporters in Houston that lunar module pilot Edwin 'Buzz' Aldrin had originally been scheduled to become the first man to set foot on the Moon during the upcoming Apollo 11 mission, but a change was made to Armstrong sometime in April or 1969.")
+}
+
+function junTwentySeven() {
+  alert("Life Magazine publishes photographs of 241 Americans killed in Vietnam in one week. Some were among the 72 killed at the 10-day battle of Hamburger Hill from May 10-20.")
+}
+
+function junTwentyNine() {
+  alert("Chicago Cubs outfielder Billy Williams breaks Stan Musial's National League record for consecutive games played as Chicago sweeps St. Louis in a double-header, 3-1 and 12-1.")
+}
+
+function junThirty() {
+  alert("Nigeria blocked most humanitarian aid to the starving residents of the breakaway Republic of Biafra by telling the Red Cross and 19 other relief agencies that they would no longer be permitted to send relief planes to the starving Biafran population.")
+}
