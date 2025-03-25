adaa6d65a0b5c4b0750fd686e5a06e0411710606
Daniel Pelstring <danielpelstring@macbookpro.lan>
2025-03-24 21:07:17 -0400

    added alerts to the jul.js file for use on the July calendar page.

--- js/jul.js
+++ js/jul.js
@@ -1,3 +1,119 @@
-function julyTwentyEight() {
-  alert("Shannon Neumann's Birthday")
+function julOne() {
+  alert("After an argument over a botched drug deal, Charles Manson shoots dealer Bernard 'Lotsapoppa' Crowe. The gun used would later be used in the murder of Sharon Tate and 4 others in August.")
+}
+
+function julTwo() {
+  alert("The newest casino and resort in Las Vegas, the International Hotel, opened to guests. Barbra Streisand performed the first concert date to inaugurate the 1,510-room hotel, at the time the tallest building (30 stories) in Nevada.")
+}
+
+function julThree() {
+  alert("A press release from Stanford University in Palo Alto, California, announced the existence of what would become the internet, with the note 'Stanford University and the Stanford Research Institute, Menlo Park, will be tied in this fall to a nationwide network that for the first time will link together computers of different makes and machine 'languages.''")
+}
+
+function julFour() {
+  alert("Michael Mageau, 19, became the first person to survive a murder attempt by a man who would become known as the 'Zodiac Killer'")
+}
+
+function julFive() {
+  alert("The crew of Apollo 11 told a press conference in Houston that they had given names to the two lunar spacecraft. 'Columbia' and 'The Eagle'.")
+}
+
+function julSix() {
+  alert("The final vestige of the penny arcade era was quietly retired as the last known one-cent machine, a 'fortune teller' that dispensed tickets for one penny, was removed and replaced by a newer version that cost five cents.")
+}
+
+function julSeven() {
+  alert("French joined English as one of the two official languages of Canada as the House of Commons approved the Official Languages Act.")
+}
+
+function julEight() {
+  alert("The first of 25,000 American troops to be withdrawn from the Vietnam War arrived at McChord Air Force Base in Washington, south of Seattle.")
+}
+
+function julNine() {
+  alert("Rock group Vanilla Fudge arrived four hours late for their concert at the Blossom Amphitheater in Northampton Township outside of Cleveland, after driving to North Hampton, Ohio, population 489, and located 200 miles away.")
+}
+
+function julTen() {
+  alert("At 8:00pm, the countdown began at five days, 13 hours and 32 minutes for the 9:32 a.m. launch on July 16 of the Apollo 11 mission.")
+}
+
+function julEleven() {
+  alert("Philips Records released David Bowie's song 'Space Oddity' (about a fictional astronaut, 'Major Tom') in conjunction with the expected launch of the Apollo 11 mission to the Moon.[62] Bowie had recorded the song three weeks earlier.")
+}
+
+function julTwelve() {
+  alert("Nearly four months after its cancellation by NBC, Star Trek was introduced to British television viewers. Its run on BBC-1.")
+}
+
+function julThirteen() {
+  alert("Actor Ken Jeong (Kendrick Kang-Joh Jeong) is born in Detroit, MI.")
+}
+
+function julFourteen() {
+  alert("The 'War on Drugs' began as President Nixon sent a message to Congress calling for 'a comprehensive legislative proposal' to control narcotics and to 'place in a single statute, a revised and modern plan for control'.")
+}
+
+function julFifteen() {
+  alert("One month before the Woodstock Festival rock concert was to take place, the zoning board of the town of Wallkill, NY, banned Woodstock Ventures from using land that the organizers had leased and cleared.")
+}
+
+function julSixteen() {
+  alert("At 9:32am Apollo XI launches from Pad 39A at the Kennedy Space Center in Florida.")
+}
+
+function julSeventeen() {
+  alert("The Soviet Union's uncrewed lunar probe Luna 15 entered lunar orbit two days ahead of the crewed Apollo 11 mission.")
+}
+
+function julEighteen() {
+  alert("Senator Edward Kennedy accidentally drives off a bridge on Chappaquiddick Island in Massachusetts. Killed in the accident was the late Robert Kennedy's secretary, Mary Jo Kopechne. Kennedy would not report the accident until the following day.")
+}
+
+function julNineteen() {
+  alert("Apollo XI enters lunar orbit.")
+}
+
+function julTwentyOne() {
+  alert("Luna 15, the uncrewed Soviet lunar probe, crashes on the Moon.  It is roughly 500 miles away from the Apollo XI landing site.")
+}
+
+function julTwentyTwo() {
+  alert("American cigarette manufacturers agreed to stop advertising on radio and television after September 30, 1970, in return for assurance from the government that warnings on the side of cigarette packages would not be sought until July 1971.")
+}
+
+function julTwentyThree() {
+  alert("Americans in several groups (members of Congress, journalists, physicians, professional teachers, college students, scholars with a post-graduate degree, scientists, and representatives of the American Red Cross) were permitted to travel to the People's Republic of China for the first time in almost 20 years.")
+}
+
+function julTwentyFour() {
+  alert("Apollo XI splashes down in the South Pacific Ocean and is retrieved by the USS Hornet.")
+}
+
+function julTwentyFive() {
+  alert("One week after Chappaquiddick, Ted Kennedy went on television to ask his Massachusetts constituents to give him 'your advice and opinion' about whether he should resign his office. The three American television networks interrupted their regular programming to broadcast the 12-minute address nationwide.")
+}
+
+function julTwentySix() {
+  alert("A 6.4 magnitude earthquake struck the city of Shantou in China. The Communist government did not acknowledge the tremor, but the Hong Kong Times, a Chinese-language paper in what was then a British colony, reported that more than 3,000 people were killed.")
+}
+
+function julTwentySeven() {
+  alert("Gary Hinman, a musician and part-time drug dealer, is killed by Bobby Beausoleil, an associate of Charles Manson and 'The Manson Family'.")
+}
+
+function julTwentyEight() {
+  alert("Our classmate, Shannon Neumann's Birthday.")
+}
+
+function julTwentyNine() {
+  alert("In a special election in Greene County, Alabama, African-American candidates won control of both the county commission and the school board of the majority-black, but minority-ruled county.")
+}
+
+function julThirty() {
+  alert("Centerfielder Bobby Tolan goes 3 for 4 with a double as the Cincinnati Reds beath the expansion Montreal Expos, 5-3 at Crosley Field in Cincinnati, OH.")
+}
+
+function julThirtyOne() {
+  alert("Elvis Presley returned to live performances for the first time since 1961, at the International Hotel in Las Vegas, NV.")
 }
